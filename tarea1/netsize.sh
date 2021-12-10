2596b1fbf1e5391e38f545ec52af2d3fd5f2d536
Jhimys25 <94380843+JhimyR@users.noreply.github.com>
2021-12-09 23:23:18 -0500

    Update netsize.sh
    Añadi información sobre los cursos básicos para instalar R, RStudio

--- tarea1/netsize.sh
+++ tarea1/netsize.sh
@@ -1,1 +1,66 @@
+# TAREA 1 - BIOINFORMÁTICA :+1:
 
+Tutorial de conceptos básicos para aprender R
+
+## Tutorial de R para principiantes. Consejos para empezar con buen pie
+-  No lo dejes para otro día. No procrastines más lo inevitable. Es momento de empezar a usar el potente software de Esdatística: R. Es el momento que te tires a la piscina. En este post te muestro el tutorial para principiantes de R.
+-  Para empezar a usar R es obligatorio empezar también con RStudio. No te asustes es el mismo software: R es el corazón, es la máquina de cálculo, es la calculadora. RStudio es el cuerpo, es la interfaz, es el vestido, el traje.
+-  La curva de aprendizaje es pronunciada. No te voy a engañar, pero puedes sacar mucho provecho de esta herramienta.
+-  Seguro que has oído hablar de este software porque es el más famoso para el análisis de datos y uso de la estadística. La verdad es que tiene mucho potencial. Pero sino es así te hago una breve explicación del software.
+
+### Es gratuito !  NO TE LO PIERDAS :clown_face:
+
+                      Tabla de contenido [ocultar]
+
+El corazón: R
+EL cuerpo: Rstudio
+Mini- tutorial de instalación de R y Rstudio
+Ejecutar tu primer script de R
+
+
+                       EL corazzón: R
+
+Para instalar R es muy sencillo. Simplemente ve al <https://cran.r-project.org/bin/windows/base/> y escoge el archivo apropiado para tu sistema operativo.
+
+                       El Cuerpo: Rstudio
+
+Basicamente es una interfas de R.Por eso nacio RStudio es muy parecida a Matlab y muy intituiva.
+Vamos a instalar RStudio.
+Esta es la <https://www.rstudio.com/> puedes descargarte RStudio y algunas packages más comerciales.
+Existen dos productos:
+ * RStudio desktop, para trabajar con RStudio en PC.
+ * RStudio server, para tener RStudio en tu servidor.
+                       
+                       Mini- tutorial de instalacion de R y RStudio
+Lo de instalar es muy fácil pero quiero que veas como se organiza R en tu PC y que cosas importantes.
+hay que tener en cuenta cuando estamos utilizando RStudio.
+
+Aqui esta el link <https://youtu.be/IyJuTU82ikM> ahi te explica detalladamente como debes instalar.
+
+                       Ejecuta tu primer script de R
+Te dejo otro vídeo con los consejos para que ejecutes tu primer código de R . (Lo típico que no te dice nadie y que es super útil)
+
+Estos son los truquitos que te enseño:
+
+Comprobar en que fichero estás trabajando.
+Mirar que todos los packages que estés utilizando los tengas instalados y cargados.
+Para ejecutar lineas de código desde el RScript estan fácil como seleccionar la línea. Una vez seleccionada pulsa Ctrl + Enter.
+Te enseño a instalar y cargar packages.
+
+El link del vide <https://youtu.be/iRJE-GBwU4k>
+Por si te interesa algo más aqui estan curso como debes instalar Python <https://www.youtube.com/watch?v=DAdRO6ByBoU>
+¡Espero te haya gustado y te animes!
+
+Aqui  tienes la web <https://conceptosclaros.com/instalar-r-primeros-pasos/>
+
+:clown_face: Tu unica oportunidad no la desperdicies :+1: :shipit:
+
+   :+1:
+## Descubre lo genial que es el mundo de la programación
+   - Ahora si viene lo bueno
+## UN TUTORIALISIMO COMO SUPERAR A TU EX , SIN QUE TE DUELA TANTO EL CORAZÓN
+   - Pri mero debes amarte a ti mismo bro , si te quieres  nunca buscaras a tu ex .
+   - Como te QUEDASTE 💩  
+Un video para que te motives y sobre todo nada esta perdido hasta intentar el ultimo recurso <https://www.youtube.com/watch?v=fBNplVDb5mw>
+## Esto ha sido todo amigos :+1: :heart:
+  :cat: :zap:
