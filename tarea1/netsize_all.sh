a93f36c97cee4a9e8eeb1776b7922aa1f750fcf6
Jhimys25 <94380843+JhimyR@users.noreply.github.com>
2021-12-10 16:41:43 -0500

    Update netsize_all.sh
    Agrege contenido sobre la actividad plants

--- tarea1/netsize_all.sh
+++ tarea1/netsize_all.sh
@@ -1,7 +1,7 @@
 # Tarea del ejercicio 1.10.3 Parte 2
 
 1.-Plants-Pollinator Networks  
-  -
+  - These can be represented as rectangular matrices where the rows are pollinator. 
   -
   -
 # :heart:
